21dbc107736ff9d237f76926330b69e4fc3d91c0
Marigaby Oropeza <marigaby091012@gmail.com>
2016-10-22 22:47:01 -0400

    Estoy haciendo una prueba :star: :poop:

---
+++ ejercicio02.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="utf-8">
+		<title>Ejercicio 2</title>
+	</head>
+	<body>
+		<p>Probando con PHP</p>
+		<hr>
+	</body>
+</html>
+
+<?php
+	$meses = array("Nombre" => "Marigaby", "Edad" => "25", "Mes" => "Julio, 09", "Año" => "1991");
+
+	foreach ($meses as $key => $value) {
+		echo "Info: $key => $value" . "<br>";	
+	}
+
+?>
